67e2dd2f23e921da51a28f659b15bbb71c745061
Andrei Shingirii <49076614+Featuredutka@users.noreply.github.com>
2020-10-26 16:01:47 +0300

    Comments added

--- 1236.py
+++ 1236.py
@@ -1,26 +1,27 @@
-primarymessage = input()
+primarymessage = input()  # Getting two lines - without and with space
 modifiedmessage = input()
 
-zerokeypoint = modifiedmessage[:2]
-zerokeypoint = int(zerokeypoint, 16)
-
-spacevalue = 0x20
-key = zerokeypoint ^ spacevalue
+zerokeybyte = modifiedmessage[:2]  # Zerokeybyte is used to decrypt 'space' from second message
 modifiedmessage = modifiedmessage[2:]
+zerokeybyte = int(zerokeybyte, 16)
+
+spacevalue = 0x20  # As we know 'space' position - we can get first key byte for further decrypting
+key = zerokeybyte ^ spacevalue
 answer = hex(key)[2:]
 
-for i in range(0, int(len(modifiedmessage)/2)):
-    msg = int(primarymessage[:2], 16)
+for i in range(0, int(len(modifiedmessage)/2)):  # Loop for getting next piece of key using previous one
+    encrmsg = int(primarymessage[:2], 16)
     primarymessage = primarymessage[2:]
-    primmsg = key ^ msg
+    decrmsg = key ^ encrmsg
 
-    msg = int(modifiedmessage[:2], 16)
+    encrmsg = int(modifiedmessage[:2], 16)
     modifiedmessage = modifiedmessage[2:]
 
-    key = primmsg ^ msg
+    key = decrmsg ^ encrmsg
     if len(hex(key)[2:]) == 1:
         answer = answer + '0' + hex(key)[2:]
     else:
         answer = answer + hex(key)[2:]
 
-print(answer.upper())
+print(answer.upper())  # Formatting answer to upper key as it required for the task
+
